4e39f8d1fce890d18b0e4a9d519f7f4d178f02c9
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-11-30 15:47:13 -0600

    test/control split

--- assignment_script.py
+++ assignment_script.py
@@ -1,6 +1,11 @@
 # import libraries
 import pandas as pd
 import pymc as pm
+import numpy as np
 
 #import data
 data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/cookie_cats.csv")
+
+# split into control/test groups
+control = data[data["version"] == "gate_30"]
+test = data[data["version"] == "gate_40"]
