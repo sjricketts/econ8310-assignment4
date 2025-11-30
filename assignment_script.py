92d8e796d8b2cca29f369e08f891eb0394a518df
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-11-30 15:03:43 -0600

    import libraries and data

---
+++ assignment_script.py
@@ -0,0 +1,6 @@
+# import libraries
+import pandas as pd
+import pymc as pm
+
+#import data
+data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/cookie_cats.csv")
