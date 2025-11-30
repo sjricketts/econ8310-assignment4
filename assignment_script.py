070cc089da69de61580c9102ed1d2d59c593f181
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-11-30 15:52:26 -0600

    retention splits

--- assignment_script.py
+++ assignment_script.py
@@ -9,3 +9,9 @@ data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/maste
 # split into control/test groups
 control = data[data["version"] == "gate_30"]
 test = data[data["version"] == "gate_40"]
+
+#split by retention day
+control_1 = control["retention_1"].values
+control_7 = control["retention_7"].values
+test_1 = test["retention_1"].values
+test_7 = test["retention_7"].values 
