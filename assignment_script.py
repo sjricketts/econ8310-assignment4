d4bc0243664050d97f48bcc0cef3a2255d5072dc
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-12-01 19:07:21 -0600

    add priors and probs

--- assignment_script.py
+++ assignment_script.py
@@ -18,3 +18,13 @@ test_7 = test["retention_7"].values
 
 # PyMC model
 with pm.Model() as model:
+  # priors that are not known
+  p_control = pm.Beta("p_control", alpha=1, beta=1)
+  p_test = pm.Beta("p_test", alpha=1, beta=1)
+
+with model:
+  # use Bernoulli for probabilities
+  # maybe should be binomial instead???
+  obs_control = pm.Bernoulli("obs_control",p=p_control, observed=control_1)
+  obs_test = pm.Bernoulli("obs_test",p=p_test, observed=test_1)
+
