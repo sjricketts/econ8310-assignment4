c94f17ec290c89494c551918d04dc022be22873c
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-12-02 20:40:10 -0600

    Clean up code; add comments

--- assignment_script.py
+++ assignment_script.py
@@ -16,31 +16,9 @@ control_7 = control["retention_7"].values
 test_1 = test["retention_1"].values
 test_7 = test["retention_7"].values 
 
-# PyMC model
-# with pm.Model() as model:
-#   # priors that are not known
-#   p_control = pm.Beta("p_control", alpha=1, beta=1)
-#   p_test = pm.Beta("p_test", alpha=1, beta=1)
 
-# with model:
-#   # use Bernoulli for probabilities
-#   # maybe should be binomial instead???
-#   obs_control = pm.Bernoulli("obs_control",p=p_control, observed=control_1)
-#   obs_test = pm.Bernoulli("obs_test",p=p_test, observed=test_1)
-
-# with model:
-#   # differences
-#   diff_raw = pm.Deterministic("diff_raw", p_test - p_control)
-#   diff_percent = pm.Deterministic("diff_percent", (p_test - p_control) / p_control)
-
-# with model:
-#   # take samples
-#   trace = pm.sample(draws=2000, tune=2000, target_accept=0.95, random_seed=311)
-
-
-# create function for 1 day
+# function for 1 day retention
 def ab_1(obs_control, obs_test):
-
   # PyMC model
   with pm.Model() as model:
 
@@ -64,22 +42,15 @@ def ab_1(obs_control, obs_test):
 # call function for 1 day rentention
 retention1_mod, retention1_trace = ab_1(control_1, test_1)
 
-# retention1_trace["p_control"]
-# retention1_trace["p_control"].mean()
-# retention1_trace["p_test"].mean()
-# retention1_trace["diff_percent"].mean()
-# retention1_trace["diff_raw"].mean()
-
-print("Retention for 1 Day\n")
+# print results
 print("control: ", retention1_trace["p_control"].mean())
 print("test: ", retention1_trace["p_test"].mean())
 print("raw difference: ",retention1_trace["diff_raw"].mean())
 print("percent difference: ",retention1_trace["diff_percent"].mean()*100)
 
 
-# create function for 7 day
+# function for 7 day retention
 def ab_7(obs_control, obs_test):
-
   # PyMC model
   with pm.Model() as model:
 
@@ -100,9 +71,11 @@ def ab_7(obs_control, obs_test):
 
   return model, trace
 
+
+# call function for 7 day rentention
 retention7_mod, retention7_trace = ab_7(control_7, test_7)
 
-# retention7_trace["p_control"].mean()
+print("Retention for 7 Day\n")
 print("control: ", retention7_trace["p_control"].mean())
 print("test: ", retention7_trace["p_test"].mean())
 print("raw difference: ",retention7_trace["diff_raw"].mean())
