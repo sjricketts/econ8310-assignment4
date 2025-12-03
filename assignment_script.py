0ac6d11aa28453babd68dbc75c29c3ed7057056f
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-12-02 20:12:47 -0600

    Separate functions

--- assignment_script.py
+++ assignment_script.py
@@ -38,16 +38,16 @@ test_7 = test["retention_7"].values
 #   trace = pm.sample(draws=2000, tune=2000, target_accept=0.95, random_seed=311)
 
 
-# create function
-def bayesian_ab(obs_control, obs_test):
-  
+# create function for 1 day
+def ab_1(obs_control, obs_test):
+
   # PyMC model
   with pm.Model() as model:
-    
+
     # priors that are not known
     p_control = pm.Beta("p_control", alpha=1, beta=1)
     p_test = pm.Beta("p_test", alpha=1, beta=1)
-    
+
     # Bernoulli probabilities
     obs_control = pm.Bernoulli("obs_control",p=p_control, observed=control_1)
     obs_test = pm.Bernoulli("obs_test",p=p_test, observed=test_1)
@@ -58,17 +58,37 @@ def bayesian_ab(obs_control, obs_test):
 
     # samples
     trace = pm.sample(draws=2000, tune=2000, target_accept=0.95, random_seed=311, return_inferencedata=False)
-  
+
   return model, trace
 
 # call function for 1 day rentention
 retention1_mod, retention1_trace = bayesian_ab(control_1, test_1)
 
-# call function for 7 day retention
-retention7_mod, retention7_trace = bayesian_ab(control_7, test_7)
+# retention1_trace["p_control"]
+# retention1_trace["p_control"].mean()
+# retention1_trace["p_test"].mean()
+# retention1_trace["diff_percent"].mean()
+# retention1_trace["diff_raw"].mean()
 
-retention1_trace["p_control"]
-retention1_trace["p_control"].mean()
-retention1_trace["p_test"].mean()
-retention1_trace["diff_percent"].mean()
-retention1_trace["diff_raw"].mean()
+# create function for 7 day
+def ab_7(obs_control, obs_test):
+
+  # PyMC model
+  with pm.Model() as model:
+
+    # priors that are not known
+    p_control = pm.Beta("p_control", alpha=1, beta=1)
+    p_test = pm.Beta("p_test", alpha=1, beta=1)
+
+    # Bernoulli probabilities
+    obs_control = pm.Bernoulli("obs_control",p=p_control, observed=control_7)
+    obs_test = pm.Bernoulli("obs_test",p=p_test, observed=test_7)
+
+    # differences
+    diff_raw = pm.Deterministic("diff_raw", p_test - p_control)
+    diff_percent = pm.Deterministic("diff_percent", (p_test - p_control) / p_control)
+
+    # samples
+    trace = pm.sample(draws=2000, tune=2000, target_accept=0.95, random_seed=311, return_inferencedata=False)
+
+retention1_mod, retention1_trace = ab_1(control_1, test_1)
